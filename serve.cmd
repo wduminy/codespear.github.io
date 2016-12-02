e3335e22affeb2365eb9277c505aa2801a75da39
Willem Duminy <willem.duminy@gmail.com>
2016-12-02 09:44:06 +0200

    upgrade to jekyll 3.3.1

--- serve.cmd
+++ serve.cmd
@@ -1,4 +1,3 @@
-# Works only in windows
-SET PATH=c:\programs\jekyll\ruby\bin;c:\programs\jekyll\devkit\bin;c:\programs\jekyll\Python\App;%PATH%;
+rem Serving jekyll version 3.3.1
 ruby --version
 jekyll server --drafts --watch
